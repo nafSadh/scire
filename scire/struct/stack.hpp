fb0c064965cecbc53151222d82647c4f2527a041
Khan “Sadh” N Mostafa <khan AT nafSadh DOT com>
2014-06-05 16:16:53 -0400

    stack implementation (initial code)

---
+++ scire/struct/stack.hpp
@@ -0,0 +1,153 @@
+// scire/struct/stack
+
+// Copyright (c) 2014, Khan 'Sadh' Mostafa (http://nafSadh.com/Khan)
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying text at http://www.boost.org/LICENSE_1_0.txt)
+
+/*
+ scire/struct/linkedlist.hpp
+
+ SCIRE Linked List implementations
+ - SinglyList	: Singly Linked List
+ - DoublyList	: Doubly Linked List
+ - CircularList	: Circular List
+
+ */
+#ifndef SCIRE_struct_stack_HPP__
+#define SCIRE_struct_stack_HPP__
+
+#include <iostream>
+using namespace std;
+
+#ifndef SCIRE_Stack_CLASS
+#define SCIRE_Stack_CLASS
+
+namespace scire
+{
+  /**
+  * Stack class.
+  *
+  * Linked list implementation of stack. A top pointer points to first item in
+  * the stack. Each item points to next item of it and the last item points to
+  * nullptr. Items can be added (push) and removed (pop) at top of the stack.
+  */
+  template<typename Type, typename SzType = int>
+  class Stack
+  {
+   public:
+    /** initialize a Stack object, with top pointing to nullptr */
+	Stack();
+
+    /** finalize a Stack object by deleting all items from the it */
+    ~Stack();
+
+    /**
+    * get count of items in the list
+    *
+    * @return SzType        count
+    */
+    SzType Size();
+
+    /**
+    * Push a new item at the top of stack with passed <data>
+    *
+    * @param data           data to push
+    * @return bool			true on success
+    */
+    bool Push(Type data);
+
+    /**
+    * Pop (remove) an item from the top.
+    *
+    * @return bool			true on success
+    */
+    bool Pop();
+
+    Type Top();
+
+   protected:
+    /**
+    * represent a node in Singly Linked List
+    */
+    struct Node {
+     public:
+      /** data */
+      Type data;
+      /** pointer to next item */
+      Node* next;
+    };
+
+   private:
+    /** points to the top of the stack */
+    Node *top;
+
+    /** track count of items stack  */
+    SzType size;
+
+  };
+
+  template<typename Type, typename SzType>
+  Stack<Type,SzType>::Stack()
+  {
+    this->top = nullptr;
+    this->size = 0;
+  }
+
+  template<typename Type, typename SzType>
+  Stack<Type, SzType>::~Stack()
+  {
+    Node *node = this->top;
+
+    // remove all node pointers, or else memory will leak
+    while (node != nullptr) {
+      Node *next = node->next;
+      delete node;
+      node = next;
+    }
+  }
+
+  template<typename Type, typename SzType>
+  SzType Stack<Type, SzType>::Size()
+  {
+    return this->size;
+  }
+
+  template<typename Type, typename SzType>
+  bool Stack<Type, SzType>::Push(Type data)
+  {
+    Node *node = new Node();
+
+    node->data = data;
+    node->next = this->top;
+    this->top = node;
+    this->size++;
+
+    return true;
+  }
+
+  template<typename Type, typename SzType>
+  bool Stack<Type, SzType>::Pop()
+  {
+	if(top==nullptr)
+		return false; //nothing to pop
+
+	//pop existing top item
+	Node *temp = this->top;
+    this->top = this->top->next;
+    this->size--;
+    delete temp;
+    return true;
+  }
+
+
+  template<typename Type, typename SzType>
+  Type Stack<Type, SzType>::Top()
+  {
+	if(this->top == nullptr) return NULL;
+
+	return this->top->data;
+  }
+}
+#endif SCIRE_SinglyList_CLASS
+
+#endif SCIRE_struct_stack_HPP__
