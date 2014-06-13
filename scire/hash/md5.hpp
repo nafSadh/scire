77d5d5256e91dcdd264cfd538972451f6898fbc3
nafSadh <khan@nafsadh.com>
2014-06-13 02:02:01 -0400

    MD5 class primary dec

--- scire/hash/md5.hpp
+++ scire/hash/md5.hpp
@@ -19,6 +19,8 @@
 #ifndef SCIRE_hash_md5_HPP
 #define SCIRE_hash_md5_HPP
 
+#include <cstdint>
+
 namespace scire
 {
 #ifndef SCIRE_MD5_CLASS
@@ -26,15 +28,26 @@ namespace scire
 
   /**
   */
-  template<typename SzType = size_t>
+  template<typename SzType= size_t, typename WordType = uint32_t,
+           typename ByteType = uint8_t, typename SignedByteType = int8_t>
   class MD5
   {
    public:
+    MD5();
     void Reset();
-    SzType BitLen();
-    SzType ByteLen();
-    SzType DigestSize();
-    SzType
+
+    void Update(const ByteType *buf, SzType lenght);
+    void Update(const SignedByteType *buf, SzType lenght);
+
+    const WordType* DigestWords();
+    const ByteType* DigestBytes();
+    void Digest(ByteType *digest);
+    void Digest(WordType *digest);
+    void Digest(WordType &A, WordType &B, WordType &C, WordType &D);
+
+    static SzType BitLen();
+    static SzType ByteLen();
+    static SzType DigestSize();
   };
 
 #endif SCIRE_MD5_CLASS
