aad33ecbb760d293afd7a652fd6559f7947c508e
nafSadh <khan@nafsadh.com>
2014-08-06 02:31:41 -0400

    no message

--- vs-proj/Source.cpp
+++ vs-proj/Source.cpp
@@ -1,41 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <algorithm>
 using namespace std;
 
 class Solution
 {
+  vector<vector<int>> mat;
+  int rowc;
+  int colc;
  public:
-  int threeSumClosest(vector<int> &num, int target)
+  int searchForRow(int low, int high, int target)
   {
-    int delta = INT_MAX;
-    int close = INT_MAX;
-
-    sort(num.begin(), num.end());
-    int n = num.size();
-
-    for (int i = 0; i <= (n - 3); i++) {
-      int a = num[i];
-      int j = i + 1;
-      int k = n - 1;
-      while (j<k) {
-        int d = a + num[j] + num[k];
-        if (abs(d - target)<delta) {
-          close = d;
-          delta = abs(d - target);
-        }
-        if (d > target) k--;
-        else j++;
-      }
+    if (low > high) return -1;
+    int mid = (low + high) / 2;
+    if (mat[mid][0] <= target && target <= mat[mid][colc - 1]) {
+      return mid;
     }
-    return close;
+    if (target<mat[mid][0]) {
+      return searchForRow(low, mid - 1, target);
+    } else {
+      return searchForRow(mid + 1, high, target);
+    }
+  }
+
+  bool findInRow(int r, int target)
+  {
+    return binary_search(mat[r].begin(),mat[r].end(), target);
+  }
+
+  bool searchMatrix(vector<vector<int> > &matrix, int target)
+  {
+    rowc = matrix.size();
+    colc = matrix[0].size();
+    mat = matrix;
+
+    //find the row
+    int row = searchForRow(0, rowc-1, target);
+    if (row<0) return false;
+    return findInRow(row, target);
   }
 };
 
 int main()
 {
-  vector<int> x {0, 2, 1, -3};
+  vector<int> x {1};
+  vector<int> y {3};
+  vector<vector<int>> v;
+  v.push_back(x);
+  v.push_back(y);
   Solution s;
-  cout << s.threeSumClosest(x, 1);
+  cout << s.searchMatrix(v, 3);
   return 0;
 }
