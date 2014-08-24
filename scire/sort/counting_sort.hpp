07b63e82bd60403df51347b08a86dd269317ca33
nafSadh <khan@nafsadh.com>
2014-08-24 00:54:59 -0400

    Init CountingSort

---
+++ scire/sort/counting_sort.hpp
@@ -0,0 +1,37 @@
+// scire/sort/counting_sort
+
+// Copyright (c) 2014, Khan 'Sadh' Mostafa (http://nafSadh.com/Khan)
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying text at http://www.boost.org/LICENSE_1_0.txt)
+
+/**
+ scire/sort/counting_sort.hpp
+
+ scire <thing> implementation:
+ - CountingSort	            : very brief description
+
+ other required scire files:
+  none
+
+ author:
+  ~nafSadh
+ */
+#ifndef SCIRE_sort_counting_sort_HPP
+#define SCIRE_sort_counting_sort_HPP
+
+namespace scire
+{
+
+#ifndef SCIRE_CountingSort_CLASS
+#define SCIRE_CountingSort_CLASS
+
+  /**
+  */
+  class CountingSort
+  {
+
+  };
+
+#endif//SCIRE_CountingSort_CLASS
+}
+#endif//SCIRE_sort_counting_sort_HPP
