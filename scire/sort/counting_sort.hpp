5f8e01363da2b9855c8ac678cbe1a9532edbe4e4
nafSadh <khan@nafsadh.com>
2014-08-24 23:52:44 -0400

    Counting sort for array

--- scire/sort/counting_sort.hpp
+++ scire/sort/counting_sort.hpp
@@ -27,9 +27,45 @@ namespace scire
 
   /**
   */
+
   class CountingSort
   {
+   public:
+    template<typename InT, typename SzT=size_t>
+    static void Array(InT* A, SzT n, InT lower, InT upper)
+    {
+      //Allocate
+      InT *B = new InT[n];
+      SzT span = upper - lower + 1;
+      InT *c = new InT[span];
+      SzT i;
 
+
+      //Init count array
+      for (i = 0; i < span; i++) {
+        c[i] = 0;
+      }
+
+      //copy array and count
+      for (i = 0; i < n; i++) {
+        B[i] = A[i];
+        c[A[i] - lower]++;
+      }
+
+      //prefix-sum
+      for (i = 1; i < span; i++) {
+        c[i] = c[i] + c[i - 1];
+      }
+
+      //copy sorted
+      for (i = n-1; i >=0 ; i--) {
+        SzT x = c[(B[i] - lower)]-1;
+        if (x >= 0 && x<n) {
+          A[x] = B[i];
+          c[(B[i] - lower)]--;
+        }
+      }
+    }
   };
 
 #endif//SCIRE_CountingSort_CLASS
