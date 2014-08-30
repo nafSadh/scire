3bfee2c13837c0357dfd1cf6c80fae005a780664
nafSadh <khan@nafsadh.com>
2014-08-29 22:57:01 -0400

    play

--- vs-proj/Source.cpp
+++ vs-proj/Source.cpp
@@ -14,30 +14,45 @@ using namespace std;
 
 int main()
 {
-  unsigned seed = 75545;
-  std::mt19937 generator(seed);
-  //int A[1000];
-  vector<int> A=vector<int>(1000);
-  for (int i = 0; i < 1000; i++) {
-    A[i] = generator()%100;
-  }
-  for (int i = 0; i < 1000; i++) {
-    cout << A[i] << ' ';
-    if (i % 20 == 19)cout << endl;
-  }
+  /*
+   unsigned seed = 75545;
+   std::mt19937 generator(seed);
+   //int A[1000];
+   vector<int> A=vector<int>(1000);
+   for (int i = 0; i < 1000; i++) {
+     A[i] = generator()%100;
+   }
+   for (int i = 0; i < 1000; i++) {
+     cout << A[i] << ' ';
+     if (i % 20 == 19)cout << endl;
+   }
+   cout << endl;
+   scire::CountingSort<int>::Vector(A, 1000, 0, 99);
+
+   for (int i = 0; i < 1000; i++) {
+     cout << A[i] << ' ';
+     if (i % 20 == 19)cout << endl;
+   }
+   */
+  for(int i=0 ; i<5 ; printf("%llx\n",pow(8,i)) , i++);
   cout << endl;
-  scire::CountingSort<int>::Vector(A, 1000, 0, 99);
-
-  for (int i = 0; i < 1000; i++) {
-    cout << A[i] << ' ';
-    if (i % 20 == 19)cout << endl;
+  printf("%llx\n", -1.0);
+  int j = 0;
+  while (j < 5) {
+    cout<<pow(10, j)<<" ";
+    cout << j << endl;
+    j++;
   }
 
-  int a = 1;
-  int *p = &a;
-  cout << p << endl;
-  p = p + 1;
-  cout << p << endl;
+  cout << "---------\n\n";
+
+  for (int i = 0; i < 5; i++) {
+
+    int k = (int)pow(10, i);
+
+    printf(" %d\n", k);
+  }
+
 
   return 0;
 }
